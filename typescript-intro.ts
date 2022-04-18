297db308a0892b694b5201317121bb61394edcc9
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-04-18 11:00:59 +0200

    Add TypeScript intro

---
+++ typescript-intro.ts
@@ -0,0 +1,17 @@
+/* 
+TypeScript is A JavaScript Superset. Its a Language building up on JavaScript. Adds new Features + Advantages to JavaScript. It cant be executed by browser environment. 
+
+TypeScript is compiled to JavaScript. TypeScript compiler compiles features to JS workarounds and possible errors are thrown. TypeScript ADDS TYPES.
+
+To install it:
+npm install typescript -g typescript
+
+tsc TYPESCRIPT-FILENAME.ts    to run the file
+
+Once we import we always import .js file because a .ts file will be compilet to .js file of same name
+
+
+TypeScript adds Next-gen JavaScript Features (compiled down for older Browsers).
+It adds Non-JS Features like Interfaces or Generics. It adds Meta_Programming Features like Decorators.
+It has Rich Configuration Options. Modern Tooling that helps even in non-TypeScript Projects.
+*/
