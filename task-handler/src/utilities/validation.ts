d2a677f12aaeb6b3e670b93f5cd97fd4e2583f79
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-04-21 17:01:22 +0200

    Update Files and folder structure using NAMESPACES

---
+++ task-handler/src/utilities/validation.ts
@@ -0,0 +1,46 @@
+namespace App {
+  //Validate
+  export interface Validatable {
+    value: string | number;
+    required?: boolean; //required?: is the same as required: boolean | undefined; ? means its optional
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+  }
+
+  export function validate(validatableInput: Validatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+      isValid =
+        isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (
+      validatableInput.minLength != null && //!= null includes both null and undefined, !== null includes just null
+      typeof validatableInput.value === "string"
+    ) {
+      isValid =
+        isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (
+      validatableInput.maxLength != null &&
+      typeof validatableInput.value === "string"
+    ) {
+      isValid =
+        isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (
+      validatableInput.min != null &&
+      typeof validatableInput.value === "number"
+    ) {
+      isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (
+      validatableInput.max != null &&
+      typeof validatableInput.value === "number"
+    ) {
+      isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+  }
+}
